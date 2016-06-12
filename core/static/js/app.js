df1f1c1dad934de4b45ae360d2e0630595ab9ead
hopaz <hopaz@163.com>
2016-06-12 21:11:49 +0800

    update

--- core/static/js/app.js
+++ core/static/js/app.js
@@ -3,3 +3,16 @@ document.addEventListener('DOMContentLoaded', init)
 function init(e) {
     console.log('hello')
 }
+
+$(document).ready(function(){
+    $('#btn-login').on('click', function(){
+        $.post('/api/login', {username:$('#login-username').val(), password:$('#login-password').val()}, function(res){
+            console.log(res)
+        });
+    });
+    $('#btn-register').on('click', function(){
+        $.post('/api/register', {username:$('#register-username').val(), password:$('#register-password').val()}, function(res){
+            console.log(res)
+        });
+    });
+});
