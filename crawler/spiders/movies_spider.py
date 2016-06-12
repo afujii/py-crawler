3c53f32ee60cdfbe65cdf544fae8c6ff742ff541
hopaz <hopaz@163.com>
2016-06-12 16:11:38 +0800

    uspatae

--- crawler/spiders/movies_spider.py
+++ crawler/spiders/movies_spider.py
@@ -4,21 +4,23 @@ import scrapy
 import json
 from core.models import db, Movie
 
-
+'''
+detail:
+https://api.douban.com/v2/movie/subject/
+'''
 class MoviesSpider(scrapy.Spider):
     name = 'MoviesSpider'
     allowed_domains = ['douban.com']
     start_urls = [
         'https://api.douban.com/v2/movie/coming_soon',
         'https://api.douban.com/v2/movie/in_theaters',
+        'https://api.douban.com/v2/movie/top250',
     ]
-
-    def __init__(self):
-        self.file = open('items.json', 'wb')
 
     def parse(self, response):
         res = json.loads(response.body)
         for subject in res['subjects']:
-            print subject['id'], subject['title']
-            # crawl_subjects(res['subjects'])
-        self.file.write(response.body + '\n')
+            m = Movie()
+            db.session.add(m)
+            r = requests.get('https://api.douban.com/v2/movie/subject/'+subject['id'])
+
