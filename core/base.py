e232912d4c8d381f6c45fd62bf645e65d46e5364
Yan.Huang <huangyan@xsky.com>
2016-06-12 20:53:12 +0800

    save

--- core/base.py
+++ core/base.py
@@ -1,36 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import request
-import json, uuid, os
 from jsonpickle import encode
-from core.orm import User, Blog
-from app import config
-
-
-path = os.path.join(os.path.abspath('.'), 'file')
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-        filename.rsplit('.', 1)[1] in config.ALLOWED_EXTENSIONS
-
-
-def save_file(file):
-    newname = str(uuid.uuid4()) + os.path.splitext(file.filename)[1]
-    file.save(os.path.join(path, newname))
-    return newname
-
-
-# preposition handler
-def request_body(func):
-    def wrapper(*args, **kw):
-        params = {}
-        if request.args:
-            params = request.args
-        elif request.form:
-            params = request.form
-        return func(params, *args, **kw)
-    return wrapper
 
 
 # filter extra attribute in objects extends flask.ext.SQLAlchemy
