73f4e5bfd49d02a6aacf773f01d3af057f394545
Yan.Huang <huangyan@xsky.com>
2016-06-13 01:15:58 +0800

    fix crawler

--- crawler/spiders/subjects_spider.py
+++ crawler/spiders/subjects_spider.py
@@ -32,19 +32,20 @@ def save_subject_detail(id, type):
 
     try:
         movie = Movie()
-        movie.title =        s.body.find(attrs={'property':'v:itemreviewed'}).get_text(),
-        movie.summary =      s.body.find(attrs={'property':'v:summary'}).get_text(),
-        movie.director =     s.body.find(attrs={'rel':'v:directedBy'}).get_text(),
-        movie.actors =       ','.join(map(lambda x: x.get_text(), s.body.find_all(attrs={'rel':'v:starring'}))),
-        movie.rating =       s.body.find(attrs={'property':'v:average'}).get_text(),
-        movie.rating_count = s.body.find(attrs={'property':'v:votes'}).get_text(),
-        movie.cover =        s.body.find(attrs={'rel':'v:image'}).attrs['src'],
-        movie.show_at =      datetime.strptime(s.body.find(attrs={'property':'v:initialReleaseDate'}).get_text()[0:10], '%Y-%m-%d'),
-        movie.crawl_at =     datetime.utcnow(),
-        movie.genres =       ','.join(map(lambda x: x.get_text(), s.body.find_all(attrs={'property':'v:genre'}))),
+        movie.title =        s.body.find(attrs={'property':'v:itemreviewed'}).get_text().strip()
+        movie.summary =      s.body.find(attrs={'property':'v:summary'}).get_text().strip()
+        movie.director =     s.body.find(attrs={'rel':'v:directedBy'}).get_text().strip()
+        movie.actors =       ','.join(map(lambda x: x.get_text().strip(), s.body.find_all(attrs={'rel':'v:starring'})))
+        movie.rating =       s.body.find(attrs={'property':'v:average'}).get_text().strip()
+        movie.rating_count = s.body.find(attrs={'property':'v:votes'}).get_text().strip()
+        movie.cover =        s.body.find(attrs={'rel':'v:image'}).attrs['src'].strip()
+        movie.show_at =      datetime.strptime(s.body.find(attrs={'property':'v:initialReleaseDate'}).get_text()[0:10], '%Y-%m-%d')
+        movie.crawl_at =     datetime.utcnow()
+        movie.genres =       ','.join(map(lambda x: x.get_text(), s.body.find_all(attrs={'property':'v:genre'}))).strip()
+
+        print 'saving movie => ', movie.title
         db.session.add(movie)
         db.session.commit()
-        print movie
     except StandardError, e:
         print 'parse subject error', e
         raise e
