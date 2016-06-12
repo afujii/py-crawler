d8583b8d7e19e8c25654d9fe33e0a04e423dca81
Yan.Huang <huangyan@xsky.com>
2016-06-12 16:03:41 +0800

    split models

--- core/__init__.py
+++ core/__init__.py
@@ -2,6 +2,9 @@
 
 from flask import Flask
 from core import settings
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(settings.APP_NAME)
 app.config.from_object(settings)
+
+db = SQLAlchemy(app)
---
+++ core/models/movie.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from core import db
+
+
+# 电影
+class Movie(db.Model):
+    def __init__(self, title='', info='', rating='', cover=''):
+        self.title = title
+        self.info = info
+        self.rating = rating
+        self.cover = cover
+
+    id = db.Column(db.Integer, primary_key=True)
+    category_id = db.Column(db.Integer)  # 分类 id
+    source_id = db.Column(db.Integer)  # 来源 id
+    title = db.Column(db.String(256))  # 电影名
+    info = db.Column(db.String(1024))  # 描述
+    director = db.Column(db.String(256))  # 导演
+    actor = db.Column(db.String(256))  # 演员
+    rating = db.Column(db.Float)  # 评分
+    cover = db.Column(db.String(256))  # 图片链接
+    crawl_time = db.Column(db.Date)  # 爬取时间
